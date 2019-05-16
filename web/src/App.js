247533484a3fef08ca156a84c55ce190b5b550f5
kingsleyokyere <kkbokyere@gmail.com>
2019-05-16 18:01:28 +0100

    update homepage tests

--- web/src/App.js
+++ web/src/App.js
@@ -1,22 +1,28 @@
 import React from 'react';
-import MovieListContainer from './containers/MovieListContainer';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+
+import Homepage from './pages/Homepage';
+import Details from './pages/Details';
 import './App.css';
 
 function App() {
   return (
-    <div className="moviegram">
-      <header className="header">
-        <input placeholder="search"/>
-      </header>
+      <Router>
+        <div className="moviegram">
+          <header className="header">
+            <input placeholder="search"/>
+          </header>
 
-        <div className="container">
-            <div className="row">
-                <div className="col-sm-12">
-                    <MovieListContainer />
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-12">
+                        <Route exact path="/" component={Homepage} />
+                        <Route exact path="/details/:id" component={Details} />
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+      </Router>
   );
 }
 
---
+++ web/src/pages/Homepage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import MovieListContainer from "../containers/MovieListContainer";
+
+const Homepage = ()=> {
+    return(<MovieListContainer/>);
+};
+
+export default Homepage;
